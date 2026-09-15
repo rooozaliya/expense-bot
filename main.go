0699075b0e2f203bff68a7ae4d96a3af1d5c94ff
rozaliya <rooozaliya@mail.ru>
2026-09-15 21:01:02 +0300

    feat: валидация

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Expense struct {
 	Amount float64
@@ -8,20 +11,26 @@ type Expense struct {
 	Description string
 }
 
-func createExpense(amount float64, category string, description string) *Expense {
-	return &Expense{
+func createExpense(amount float64, category string, description string) (Expense, error) {
+	if(amount<= 0) {
+		return Expense{}, errors.New("сумма должна быть больше нуля")
+	}
+	return Expense{
 		Amount: amount,
 		Category: category,
 		Description: description,
-	}
+	}, nil
 }
 
 
 func main() {
-	expense:= createExpense(450, "food", "dinner")
+	expense, err := createExpense(-450, "food", "dinner")
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
 
 
-	fmt.Println(expense)
 	fmt.Println("Сумма:", expense.Amount)
 	fmt.Println("Категория:", expense.Category)
 	fmt.Println("Описание:", expense.Description)
