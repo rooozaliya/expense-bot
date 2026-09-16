2cc5898d6f8dfe7e9f626ae7fdb15a74e1f5fef6
rozaliya <rooozaliya@mail.ru>
2026-09-16 20:22:51 +0300

    feat: подключение тг

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"log"
 	"os"
-	"strconv"
-	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Expense struct {
-	Amount float64
-	Category string 
+	Amount      float64
+	Category    string
 	Description string
-	Currency string
+	Currency    string
+}
+
+func createExpense(amount float64, category, description, currency string) (Expense, error) {
+	if amount <= 0 {
+		return Expense{}, fmt.Errorf("сумма должна быть больше нуля")
+	}
+
+	if category == "" {
+		return Expense{}, fmt.Errorf("категория не может быть пустой")
+	}
+
+	if description == "" {
+		return Expense{}, fmt.Errorf("описание не может быть пустым")
+	}
+
+	if currency == "" {
+		return Expense{}, fmt.Errorf("валюта не может быть пустой")
+	}
+
+	return Expense{
+		Amount:      amount,
+		Category:    category,
+		Description: description,
+		Currency:    currency,
+	}, nil
+}
+
+func (e Expense) IsValid() bool {
+	return e.Amount > 0 && e.Category != ""
+}
+
+func (e *Expense) UpdateDescription(description string) {
+	e.Description = description
 }
 
 func (e Expense) Format() string {
@@ -25,141 +58,43 @@ func (e Expense) Format() string {
 	)
 }
 
-func createExpense(amount float64, category, description, currency string) (Expense, error) {
-	if(amount<= 0) {
-		return Expense{}, fmt.Errorf("сумма должна быть больше нуля")
-	}
-
-
-	if category == "" {
-		return Expense{}, fmt.Errorf("категория не может быть пустой")
-	}
-
-	if description == "" {
-		return Expense{}, fmt.Errorf("описание не может быть пустой")
-	}
-
-
-	if currency == "" {
-		return Expense{}, fmt.Errorf("валюта не может быть пустой")
-	}
-
-
-	return Expense{
-		Amount: amount,
-		Category: category,
-		Description: description,
-		Currency:    currency,
-	}, nil
-}
-
-func readAmount(reader *bufio.Reader) (float64, error) {
-	fmt.Print("Введите сумму: ")
-
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-
-	input = strings.TrimSpace(input)
-
-	amount, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, fmt.Errorf("сумма должна быть числом")
-	}
-
-	return amount, nil
-}
-
-
-func readText(reader *bufio.Reader, prompt string) (string, error) {
-	fmt.Print(prompt)
-
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(input), nil
-}
-
-//e — это конкретный Expense, с которым работает метод. как this в пхп
-func (e Expense) IsValid() bool {
-	return e.Amount > 0 && e.Category != ""
-}
-
-//* изменение объекта, указатель
-func (e *Expense) UpdateDescription(description string) {
-	e.Description = description
-}
-
 func main() {
-	
-	reader:= bufio.NewReader(os.Stdin)
-	expenses := []Expense{}
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
 
-	for {
-		amount, err := readAmount(reader)
-
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			continue
-		}
-	
-		category, err := readText(reader, "Введите категорию: ")
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			continue
-		}
-		description, err := readText(reader, "Введите описание: ")
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			continue
-		}
-
-		currency, err := readText(reader, "Введите валюту: ")
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			continue
-		}
-	
-		expense, err := createExpense(
-			amount, 
-			category, 
-			description,
-			currency,
-		)
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			continue
-		}
-
-		expenses = append(expenses, expense)
-
-		fmt.Println("Расход добавлен:")
-
-		answer, err := readText(reader, "Добавить ещё? (y/n): ")
-
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			return
-		}
-
-		if answer != "y" {
-			break
-		}
+	if token == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN не установлен")
 	}
 
-
-	fmt.Println()
-	fmt.Println("Ваши расходы:")
-
-
-	//_, - range может вернуть две вещи: индекс + значение
-	//_ означает: это значение я намеренно игнорирую.
-	for _, expense := range expenses {
-		fmt.Println(expense.Format())
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
 	}
-	
+
+	log.Printf("Бот запущен: @%s", bot.Self.UserName)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			switch update.Message.Command() {
+			case "start":
+				msg := tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Привет! Я бот для учёта расходов.",
+				)
+
+				_, err := bot.Send(msg)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
 }
-
