c0ab43617a1fb61d68298395e29357393c387514
rozaliya <rooozaliya@mail.ru>
2026-09-16 20:02:39 +0300

    feat: сохранение трат

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Expense struct {
@@ -13,8 +16,18 @@ type Expense struct {
 
 func createExpense(amount float64, category string, description string) (Expense, error) {
 	if(amount<= 0) {
-		return Expense{}, errors.New("сумма должна быть больше нуля")
+		return Expense{}, fmt.Errorf("сумма должна быть больше нуля")
 	}
+
+
+	if category == "" {
+		return Expense{}, fmt.Errorf("категория не может быть пустой")
+	}
+
+	if description == "" {
+		return Expense{}, fmt.Errorf("описание не может быть пустой")
+	}
+
 	return Expense{
 		Amount: amount,
 		Category: category,
@@ -22,17 +35,98 @@ func createExpense(amount float64, category string, description string) (Expense
 	}, nil
 }
 
+func readAmount(reader *bufio.Reader) (float64, error) {
+	fmt.Print("Введите сумму: ")
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+
+	amount, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, fmt.Errorf("сумма должна быть числом")
+	}
+
+	return amount, nil
+}
+
+
+func readText(reader *bufio.Reader, prompt string) (string, error) {
+	fmt.Print(prompt)
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(input), nil
+}
+
+
 
 func main() {
-	expense, err := createExpense(-450, "food", "dinner")
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
+	
+	reader:= bufio.NewReader(os.Stdin)
+	expenses := []Expense{}
+
+	for {
+		amount, err := readAmount(reader)
+
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+	
+		category, err := readText(reader, "Введите категорию: ")
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+		description, err := readText(reader, "Введите описание: ")
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+	
+		expense, err := createExpense(amount, category, description)
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+
+		expenses = append(expenses, expense)
+
+		fmt.Println("Расход добавлен:")
+
+		answer, err := readText(reader, "Добавить ещё? (y/n): ")
+
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			return
+		}
+
+		if answer != "y" {
+			break
+		}
 	}
 
 
-	fmt.Println("Сумма:", expense.Amount)
-	fmt.Println("Категория:", expense.Category)
-	fmt.Println("Описание:", expense.Description)
+	fmt.Println()
+	fmt.Println("Ваши расходы:")
+
+
+	//_, - range может вернуть две вещи: индекс + значение
+	//_ означает: это значение я намеренно игнорирую.
+	for _, expense := range expenses {
+		fmt.Printf("%.2f ₽ | %s | %s\n",
+			expense.Amount,
+			expense.Category,
+			expense.Description,
+		)
+	}
+	
 }
 
